49bfb6a2f619feea9694a7041a6e239f8a407d2d
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-05-23 15:50:13 +0530

    Create variables.tf

---
+++ nsg/variables.tf
@@ -0,0 +1,18 @@
+variable "resource_group" {
+  type = string
+}
+variable "nsg_name" {
+    type = string
+}
+
+variable "nsg_tag" {
+    type = string
+}
+
+variable "nsg_rule1_name" {
+    type = string
+}
+
+variable "nsg_rule2_name" {
+    type = string
+}

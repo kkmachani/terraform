8028c60ab9eb0025432947e2044d0b4d4f62a1ee
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-05-23 15:38:25 +0530

    Create variables.tf

---
+++ vm/variables.tf
@@ -0,0 +1,60 @@
+variable "resource_group" {
+  type = string
+}
+
+variable "nic_name" {
+    type = string
+}
+
+variable "ip_name" {
+    type = string
+}
+
+variable "vm_public_ip" {
+    type = string
+}
+
+variable "subnet_id" {
+    type = string
+}
+
+variable "vm_name" {
+    type = string
+}
+
+variable "vm_size" {
+    type = string
+}
+
+variable "vm_tag" {
+    type = string
+}
+
+variable "osdisk" {
+    type = string
+}
+
+variable "computer_name" {
+    type = string
+}
+
+variable "admin_username" {
+    type = string
+}
+
+variable "admin_password" {
+    type = string
+}
+
+variable "local_file_path" {
+    type = string
+}
+
+variable "remote_directory" {
+    type = string
+}
+
+variable "nsg_id" {
+    type = string
+}
+
